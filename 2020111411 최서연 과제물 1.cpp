8038b927fa94cc19c0b8f7b4e182d48ee1477927
csy-59 <csy010509@naver.com>
2021-03-05 13:54:10 +0900

    First commit - finnishing first homework

---
+++ 2020111411 최서연 과제물 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+const int SUBJECT_NUM = 1;
+const int STUDENT_NUM = 10;
+
+int main() {
+	int scoreList[SUBJECT_NUM][STUDENT_NUM];
+
+	srand(time(0));
+
+	int sum[SUBJECT_NUM] = {0};
+
+	for(int i=0;i<SUBJECT_NUM;i++){
+		for(int j = 0;j<STUDENT_NUM;j++){
+			scoreList[i][j] = rand()%100;
+			sum[i]+=scoreList[i][j];
+		}
+	}
+
+	for(int i =0;i<SUBJECT_NUM;i++){
+		printf("The Average is %.1f\n", sum[i]);
+	}
+
+
+	return 0;
+}
