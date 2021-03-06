80884980533f8215381eef270d5468b93214b92b
csy-59 <csy010509@naver.com>
2021-03-06 18:02:54 +0900

    Third homework Success!!

---
+++ 2020111411 최서연 과제물 3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+const int CLASS_NUM = 3;
+const int SUBJECT_NUM = 3;
+const int STUDENT_NUM = 10;
+
+int main() {
+	string subjectList[SUBJECT_NUM] = { "Langage", "English", "Math" };
+	double scoreList[CLASS_NUM][SUBJECT_NUM][STUDENT_NUM];
+	double classScoreSum[CLASS_NUM][SUBJECT_NUM] = { 0 };
+	double subjectScoreSum[SUBJECT_NUM] = { 0 };
+
+	srand(time(0));
+
+	for (int Class = 0; Class < CLASS_NUM;Class++) {
+		for (int sub = 0;sub < SUBJECT_NUM;sub++) {
+			for (int stu = 0;stu < STUDENT_NUM;stu++) {
+				scoreList[Class][sub][stu] = rand() % 101;
+				classScoreSum[Class][sub] += scoreList[Class][sub][stu];
+				subjectScoreSum[sub] += scoreList[Class][sub][stu];
+			}
+		}
+	}
+
+	cout << "Followings are Average by Class & Subjects:" << endl;
+
+	for (int i = 0;i < CLASS_NUM;i++) {
+		for (int j = 0;j < SUBJECT_NUM;j++) {
+			cout << i << " - " << subjectList[j] << ": ";
+			printf("%.1f\n", classScoreSum[i][j] / STUDENT_NUM);
+		}
+	}
+
+	cout << "\nFollowings are Average by Subjects: " << endl;
+	
+	for (int i = 0;i < SUBJECT_NUM;i++) {
+		cout << subjectList[i] << ": ";
+		printf("%.1f\n", subjectScoreSum[i] / (STUDENT_NUM*CLASS_NUM));
+	}
+
+	return 0;
+}
