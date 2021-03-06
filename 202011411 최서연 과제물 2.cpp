05291333e45cc261ed151ad40f8f3128635e4860
csy-59 <csy010509@naver.com>
2021-03-06 17:21:53 +0900

    second homwork - making subject avg

---
+++ 202011411 최서연 과제물 2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+
+//과제물 1의 코드를 활용했습니다.
+
+const int SUBJECT_NUM = 3;
+const int STUDENT_NUM = 10;
+
+int main() {
+	int scoreList[SUBJECT_NUM][STUDENT_NUM];
+	string subjectList[SUBJECT_NUM] = { "Langage", "English","Math" };
+
+	srand(time(0));
+
+	double sum[SUBJECT_NUM] = { 0 };
+
+	for (int i = 0;i<SUBJECT_NUM;i++) {
+		for (int j = 0;j<STUDENT_NUM;j++) {
+			scoreList[i][j] = rand() % 101;
+			sum[i] += scoreList[i][j];
+		}
+	}
+
+	for (int i = 0;i<SUBJECT_NUM;i++) {
+		cout << "The Average of " << subjectList[i] << " is ";
+		printf("%.1f\n", sum[i] / STUDENT_NUM);
+	}
+
+
+	return 0;
+}
